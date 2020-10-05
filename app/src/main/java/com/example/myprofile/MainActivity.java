88fe51526c7426036ecf2a100be6be9a429b4559
priyal bhatewara <47683250+priyalbhatewara123@users.noreply.github.com>
2020-10-06 02:28:13 +0530

    Added login Page

--- app/src/main/java/com/example/myprofile/MainActivity.java
+++ app/src/main/java/com/example/myprofile/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.myprofile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private static int Timeout = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },Timeout);
     }
 }
